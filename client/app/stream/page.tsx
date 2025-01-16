07b7026bd93d14f4cee89f8aa718692dc8f19ad4
Ovishek Paul <ovishek.private@gmail.com>
2025-01-16 23:18:25 +0600

    Ovishek/material UI addition (#14)
    * add mat ui
    
    * done
    
    * push it

--- client/app/stream/page.tsx
+++ client/app/stream/page.tsx
@@ -5,7 +5,7 @@ import StreamDataWrapper from "./component/StreamDataWrapper";
 
 export default function Home() {
   return (
-    <div className=" font-[family-name:var(--font-geist-sans)] pt-24">
+    <div className=" font-[family-name:var(--font-geist-sans)]">
       <Navbar />
     {/* <StreamData/> */}
     <StreamDataWrapper/>
