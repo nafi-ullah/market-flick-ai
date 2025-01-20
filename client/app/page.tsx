bf093a62c5b3a45c4fbedc35565190f58e328fb8
Nafi Ullah Shafin <shafinnafiullah@gmail.com>
2025-01-20 19:01:03 +0600

    added landing navbar and footer

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,26 +1,39 @@
 "use client";
 import React, { useState } from "react";
 
-
 import HeroSection from "@/components/landings/HeroSection";
 import ProductVideoSection from "@/components/landings/VideoSection";
 import DetailedFeaturesSection from "@/components/landings/Features";
 import PricingComponent from "@/components/landings/Pricing";
 import ProductFrequentlyAskedQuestionsSection from "@/components/landings/FAQSection";
+import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
 
 
 
 export default function Home() {
 
   return (
-    <div className="font-[family-name:var(--font-geist-sans)] pt-24">
-      {/* <Navbar /> */}
-      {/* <Header/> */}
-      <HeroSection />
-              <ProductVideoSection />
-              <DetailedFeaturesSection />
-              <PricingComponent />
-              <ProductFrequentlyAskedQuestionsSection />
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <Navbar />
+      <main>
+        <section id="hero">
+          <HeroSection />
+        </section>
+        <section id="product-video-section">
+          <ProductVideoSection />
+        </section>
+        <section id="features">
+          <DetailedFeaturesSection />
+        </section>
+        <section id="pricing">
+          <PricingComponent />
+        </section>
+        <section id="faq">
+          <ProductFrequentlyAskedQuestionsSection />
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
